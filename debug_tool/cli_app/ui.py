b41800275415a84a990fd22d01466b3c3b8c748b
ropali <ropali68@gmail.com>
2023-03-21 15:38:17 +0530

    feat: improved layout

--- debug_tool/cli_app/ui.py
+++ debug_tool/cli_app/ui.py
@@ -2,44 +2,130 @@ import uuid
 from time import monotonic
 from typing import Dict, List
 
+from rich import box
+from rich.align import Align
+from rich.panel import Panel
 from textual.app import App, ComposeResult
-from textual.containers import Container
+from textual.containers import Container, Grid
 from textual.reactive import reactive
 from textual.widget import Widget
 from textual.widgets import Button, Header, Footer, Static, ListView, ListItem, Label
+from cli_app.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class TextBox(Widget):
+    def __init__(self, name: str, text: str, is_titled: bool, align: str, height: int = None) -> None:
+        super().__init__(name=name)
+        self.text = text
+        self.is_titled = is_titled
+        self.align = align
+        self.height = height
+
+    def render(self) -> Panel:
+        if self.align == "left":
+            align = Align.left
+        elif self.align == "center":
+            align = Align.center
+        else:
+            align = Align.right
+        return Panel(
+            align(self.text, vertical="middle"),
+            title=self.name if self.is_titled else None,
+            border_style="white",
+            box=box.ROUNDED,
+            height=self.height
+        )
+
+
+class InfoBox(Widget):
+    DEFAULT_CLASSES = "info-box"
+
+    def __init__(self, name: str, text: str, align: str) -> None:
+        super().__init__(name=name)
+        self.text = text
+        self.align = align
+
+    def render(self) -> Panel:
+        if self.align == "left":
+            align = Align.left
+        elif self.align == "center":
+            align = Align.center
+        else:
+            align = Align.right
+        return Panel(
+            align(self.text, vertical="middle"),
+            title=self.name,
+            border_style="white",
+            box=box.ROUNDED,
+            safe_box=True,
+            height=None
+        )
 
 
 class LeftPanel(Widget):
     items = reactive([])
 
+    DEFAULT_CSS = """
+        ListItem {
+            color: $text;
+            height: auto;
+            background: #5f6062;
+            overflow: hidden hidden;
+        }
+        ListItem > Widget :hover {
+            background: #5f6062;
+        }
+        ListView > ListItem.--highlight {
+            background: #7b7263 50%;
+        }
+        ListView:focus > ListItem.--highlight {
+            background: #7b7263;
+        }
+        ListItem > Widget {
+            height: auto;
+        }
+        """
+
     def __init__(self, items: List[Dict], **kwargs):
         super().__init__(**kwargs)
-        self.items = [ListItem(Label(item.get("title"), classes="request_item")) for item in items]
+        self.items = items
 
     def compose(self) -> ComposeResult:
+        logger.info(str(self.items))
+        self.items = [ListItem(Label(f"[{item.get('method')}] {item.get('path')}", classes="request_item")) for item in
+                      self.items]
 
-        yield Static(
-            "All Request",
-            expand=True,
-            id="left_panel_header"
-        )
-
-        yield ListView(
-            *self.items,
-            initial_index=None,
-            id="left_panel_list_view"
+        yield Container(
+            ListView(
+                *self.items,
+                initial_index=None,
+                id="left_panel_list_view"
+            ),
+            id="left_panel"
         )
 
 
 class RightPanel(Widget):
-    """A stopwatch widget."""
+
+    def __init__(self, data: Dict, **kwargs):
+        self.data = data
+        super().__init__(**kwargs)
+
+    def construct_text(self) -> str:
+        return f"HTTP Method: {self.data.get('method')} \n" \
+               f"Path: {self.data.get('path')}\n" \
+               f"Execution Time: {self.data.get('time')} ms\n" \
+               f"URL: {self.data.get('url')}\n" \
+            f"Response Code: {self.data.get('status_code')}\n"
 
     def compose(self) -> ComposeResult:
-        yield ListView(
-            ListItem(Label("4")),
-            ListItem(Label("5")),
-            ListItem(Label("6")),
-            initial_index=None,
+        yield Container(
+            InfoBox("Basics", self.construct_text(), align="left"),
+            InfoBox("Query Parameters", self.construct_text(), align="left"),
+            InfoBox("Headers", self.construct_text(), align="left"),
+            id="right_panel"
         )
 
 
@@ -59,8 +145,10 @@ class DebugApp(App):
 
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
-        yield Container(LeftPanel(items=self.data, id="my_list"), id="left_panel")
-        yield Container(RightPanel(), id="right_panel")
+        yield Container(TextBox("FastAPI Debug", "FastAPI Inspector", False, "center"), id="app_title")
+        yield LeftPanel(items=self.data)
+        yield RightPanel(self.data[0])
+
         yield Footer()
 
     def action_add_item(self):
--- debug_tool/main.py
+++ debug_tool/main.py
@@ -2,6 +2,8 @@ import datetime
 import json
 import socket
 import os
+import uuid
+
 from cli_app.ui import render_ui
 
 HOST = '0.0.0.0'  # The server's hostname or IP address
@@ -37,7 +39,8 @@ def start_server():
                             f"-------------------------Received[{datetime.datetime.now()}]---------------------------------")
                         print(json.loads(data.decode('utf-8')))
                         request_data.append(json.loads(data.decode('utf-8')))
-                        print(f"-------------------------Received[{datetime.datetime.now()}]---------------------------------")
+                        print(
+                            f"-------------------------Received[{datetime.datetime.now()}]---------------------------------")
             except Exception as e:
                 print("An error occurred:", e)
                 # Restart the program if it crashes
@@ -45,6 +48,69 @@ def start_server():
 
 
 if __name__ == '__main__':
-    render_ui(request_data)
-    # start_server()
+    data = [
+        {
+            "request_id": str(uuid.uuid4()),
+            "time": 0.123,
+            "url": "https://www.google.com",
+            "method": "GET",
+            "headers": {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Host": "www.google.com",
+            },
+            "body": "",
+            "cookies": {
+                "JSESSIONID": "",
+            },
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            "referer": "https://www.google.com/",
+            "host": "www.google.com",
+            "ip": "127.0.0.1",
+            "path": "/todo",
+            "protocol": "https",
+            "query_string": {
+                "q": "",
+                "oq": "",
+            },
+            "body_params": {
+                "q": "",
+                "oq": "",
+            },
 
+        },
+        {
+            "request_id": str(uuid.uuid4()),
+            "time": 0.123,
+            "url": "https://www.google.com",
+            "method": "POST",
+            "headers": {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Host": "www.google.com",
+            },
+            "body": "",
+            "cookies": {
+                "JSESSIONID": "",
+            },
+            "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            "referer": "https://www.google.com/",
+            "host": "www.google.com",
+            "ip": "127.0.0.1",
+            "path": "/todo",
+            "protocol": "https",
+            "query_string": {
+                "q": "",
+                "oq": "",
+            },
+            "body_params": {
+                "q": "",
+                "oq": "",
+            },
+
+        }
+    ]
+    render_ui(data)
+    # start_server()
