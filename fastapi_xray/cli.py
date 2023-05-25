c0e0c673428326e3de64f8829ab65dc93d9e4e58
ropali <ropali68@gmail.com>
2023-05-25 20:54:10 +0530

    fix: Added args details for cli 🗒️

--- fastapi_xray/cli.py
+++ fastapi_xray/cli.py
@@ -2,6 +2,7 @@ import multiprocessing
 from multiprocessing import Process
 
 import typer
+from typer import Argument
 
 from fastapi_xray.commons.logger import get_logger
 from fastapi_xray.server import start_server
@@ -13,7 +14,18 @@ logger = get_logger()
 
 
 @app.command()
-def run(host: str = "0.0.0.0", port: int = 8989, disable_log: bool = True):
+def run(
+    host: str = Argument(  # noqa :B008
+        "0.0.0.0",
+        help="Host where server will listen for incoming data for UI",
+    ),
+    port: int = Argument(  # noqa :B008
+        8989, help="Port where server will listen for incoming data for UI"
+    ),
+    disable_log: bool = Argument(  # noqa :B008
+        True, help="Generate logs for debugging."
+    ),
+):
     """Runs the UI server and X-Ray server."""
 
     logger.disabled = disable_log  # only for internal debugging
